c21478629e5ab3f08a912126da8dcb85b6170447
yazan-sul <yazan-sul@gmail.com>
2025-07-29 16:31:46 +0300

    sol exercise 4

--- exercise4.ts
+++ exercise4.ts
@@ -38,11 +38,11 @@ export const persons: Person[] = [
     { type: 'admin', name: 'Bruce Willis', age: 64, role: 'World saver' }
 ];
 
-export function isAdmin(person: Person) {
+export function isAdmin(person: Person) : person is Admin  {
     return person.type === 'admin';
 }
 
-export function isUser(person: Person) {
+export function isUser(person: Person): person is User  {
     return person.type === 'user';
 }
 
